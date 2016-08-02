e2e46f3287b4f0d9de8fde210f8c7de177e83640
lpbastardo <leandrobastardo@gmail.com>
2016-08-01 23:29:18 -0300

    projeto com autorização
    atualizações 23:00

---
+++ EstagiarioWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EstagiarioWeb.MvcApplication" Language="C#" %>
---
+++ EstagiarioWeb/Models/Computer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EstagiarioWeb.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        [Display(Name = "Marca da placa Mãe: ")]
+        public string Marcaplacamae { get; set; }
+        [Display(Name = "Marca da placa Mãe: ")]
+        public string modeloplacamae { get; set; }
+
+        [Display(Name = "Marca de Ram: ")]
+        public string capacidaderam { get; set; }
+        [Display(Name = "Quantidade de Ram: ")]
+        public int quantidaderam { get; set; }
+
+        [Display(Name = "Capacidade do Hd: ")]
+        public string capacidadehd { get; set; }
+        [Display(Name = "Quantindade de Hd: ")]
+        public string quantidadehd { get; set; }
+
+        [Display(Name = "Modelo de fone: ")]
+        public string modelofone { get; set; }
+        [Display(Name = "Marca do Fone: ")]
+        public string marcafone { get; set; }
+
+        [Display(Name = "Marca do processador: ")]
+        public string marcaprocessador { get; set; }
+        [Display(Name = "Velocidade do processador: ")]
+        public string velocidadeprocessador { get; set; }
+
+    }
+}
